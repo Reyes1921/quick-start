f1696aab2d85fab2eab88fbd482514647205be3c
Reyes Rondon <reyes192119@gmail.com>
2024-07-09 19:54:15 -0400

    changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <>
       <Header />
       <main className="flex min-h-screen flex-col items-center justify-between px-12 md:px-52 py-10 bg-[white] dark:bg-bgColor">
-        <div className="min-h-screen">TEXT</div>
+        <div className="min-h-screen">TEXT TEXT</div>
       </main>
       <Footer />
     </>
